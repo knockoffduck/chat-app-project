1e4194be6bccbe276d1abb0e22bf3a9d3fd923f9
knockoffduck <ddarm4@gmail.com>
2023-03-29 09:14:48 +0800

    first commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $("#title").click(() => {
-        alert("testing")
+    $("#create").click(() => {
+        const inputValue = $("#myInput").val();
+        $("#randomList").append(`<p id="result">${inputValue}</p>`)
+
     })
 })
