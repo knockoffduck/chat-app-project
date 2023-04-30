ecc38c6d39808290648a79f4146811f6b51545b6
tom18072001 <129664679+tom18072001@users.noreply.github.com>
2023-04-30 17:32:56 +0800

    Revert "file for chat,signup,login"
    This reverts commit 50833c64849380608c05bb1bbb6f40923bb8e949.

--- application/routes.py
+++ application/routes.py
@@ -2,42 +2,9 @@ from application import app
 from flask import render_template, request, url_for
 
 @app.route('/')
-@app.route('/homepage')
+@app.route('/index')
 def index():
-    return render_template('hompage.html')
-app.route('Logup')
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # perform login validation here
-        return redirect('/Chat')
-    else:
-        return render_template('login.html')
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        # retrieve form data
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        email = request.form['email']
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
-        date_of_birth = request.form['date_of_birth']
-        gender = request.form['gender']
-        country = request.form['country']
-
-        # do any validation or processing here
-
-        # redirect to a new page after successful signup
-        return redirect('/Chat')
-
-    # render the signup form for GET requests
-    return render_template('signup.html')
-@app.route('/Chat')
-def submit():
-    message = request.form.get("message")
-    return render_template("chat.html", message=message)
+    return render_template('index.html')
 
 #@app.route('')
 # @app.route('/button')
