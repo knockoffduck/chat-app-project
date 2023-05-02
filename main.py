57228ced6805049fdfd6276e9aaed1ef797da971
eli3005 <126839344+eli3005@users.noreply.github.com>
2023-05-02 20:49:47 +0800

    adding shell context for db

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from website import create_app
+from website import create_app, db
+from website.models import User, Chat
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Chat': Chat}
