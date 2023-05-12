9323a7ec60f5a7464083f9b6b44c2d3eda706693
tom18072001 <129664679+tom18072001@users.noreply.github.com>
2023-05-12 17:11:47 +0800

    commit

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,7 @@
 from application import app
-from flask import render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory
+
+import datetime
 
 @app.route('/')
 @app.route('/homepage')
@@ -16,25 +18,41 @@ def login():
     else:
         return render_template('login.html')
     
+
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # retrieve form data
+        # Get form data
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
-        date_of_birth = request.form['date_of_birth']
-        gender = request.form['gender']
+        dob = request.form['dob']
         country = request.form['country']
+        gender = request.form['gender']
 
-        # do any validation or processing here
+        # Validate password length
+        if len(password) < 8:
+            return "Password must be at least 8 characters long"
 
-        # redirect to a new page after successful signup
-        return redirect('/chat')
+        # Validate year of birth
+        current_year = datetime.datetime.now().year
+        dob_year = int(dob.split('-')[0])
+        if dob_year > current_year or dob_year < (current_year - 100) or dob_year > 2022:
+            return "Invalid year of birth"
 
-    # render the signup form for GET requests
+        # # Create user account
+        # user = User(first_name=first_name, last_name=last_name, email=email, password=password, dob=dob, country=country, gender=gender)
+        # db.session.add(user)
+        # db.session.commit()
+
+        # Send email confirmation
+        # ...
+
+        return "User account created successfully"
+
     return render_template('signup.html')
 
 @app.route('/chat')
@@ -44,3 +62,10 @@ def submit():
     #     return render_template("chat.html", message=message)
     # else:
     return render_template("chat.html")
+@app.route('/desktop-1')
+def submit1():
+    # if request.method == 'POST':
+    #     message = request.form.get("message")
+    #     return render_template("chat.html", message=message)
+    # else:
+    return render_template("desktop-1.html")
