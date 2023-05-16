6d42a0d331e646f745de1aec7611131fd63f30c0
tom18072001 <129664679+tom18072001@users.noreply.github.com>
2023-05-16 16:12:42 +0800

    Revert "updated"
    This reverts commit 00162650736f71a1559bdefccef2655870a70f1d.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from website import create_app, db
-from website.models import User, Chat
+from application import app
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import os
 
-app = create_app()
+CORS(app)
+
+#@app.route('/home')
+#def home():
+#    return 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Chat': Chat}
+    port = os.environ.get('PORT', 5000)
+    app.run(debug = True, host = '0.0.0.0', port = port)
---
+++ application/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+app = Flask(__name__, static_folder='static')
+
+
+from application import routes
